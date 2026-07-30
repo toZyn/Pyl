89074e3df6224d8013f7d3cfd6767b9c3e87b83f
⊱: ̗̀SσყMαყƈσʟ࿐ <karatekidamericatv@gmail.com>
2026-07-30 13:23:57 -0500

    Add Pyl framework

---
+++ src/core/types.ts
@@ -0,0 +1,16 @@
+import type { Api } from "gramjs";
+
+export type PylContext = {
+  event: any;
+  client: any;
+  args: string[];
+  reply: (text: string) => Promise<Api.Message>;
+};
+
+export type PylCommand = {
+  name: string;
+  title: string;
+  description: string;
+  aliases?: string[];
+  execute: (ctx: PylContext) => Promise<void> | void;
+};
